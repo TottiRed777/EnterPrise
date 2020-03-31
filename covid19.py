b739c469d11d7ea764c15a6fd7a79e6dff56f82f
TottiRed777 <TottiRed777@163.com>
2020-03-31 12:08:40 +0800

    企业性或地区性信息

--- covid19.py
+++ covid19.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 import json
 import pymysql
 
-#商事主体信息
+#新冠肺炎个体信息
 class COVID19:
         def intoMysql(self):
             conn = pymysql.connect(host='192.168.1.108', user='root', password='root', db='enterprise', charset="utf8")
@@ -48,42 +48,42 @@ class COVID19:
             print("总共有" + str(TotalNum) + "条数据")
             # 获取页数（以一页1000条数据作为分母）
             Page = TotalNum // 200
-            print("需查询至第" + str(Page) + "页")
-            for page in range(1, Page+2):
-                url="https://opendata.sz.gov.cn/api/29200_01503668/1/service.xhtml?page="+str(page)+"&rows=200&appKey=d8445b1959af407b9697b67798330930"
-                html=urlopen(url)
-                hjson=json.loads(html.read())
-                #获取data数据
-                data = hjson['data']
-                print(data)
-
-                for row in range(0,200):
-                    fbrq=data[row]['fbrq']
-                    fbusj=data[row]['fbusj']
-                    blh=data[row]['blh']
-                    nl=data[row]['nl']
-                    xb=data[row]['xb']
-                    jzd=data[row]['jzd']
-                    yqtblgx=data[row]['yqtblgx']
-                    zwhsjqj=data[row]['zwhsjqj']
-                    lssj=data[row]['lssj']
-                    fbingsj=data[row]['fbingsj']
-                    rysj=data[row]['rysj']
-                    rbyy=data[row]['rbyy']
-                    bk=data[row]['bk']
-                    bzzzytjd = data[row]['bzzzytjd']
-                    sql2 = '''
-                        insert into covid19(fbrq,fbusj,blh,nl,xb,jzd,yqtblgx,zwhsjqj,lssj,fbingsj,rysj,rbyy,bk,bzzzytjd)
-                        values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
-                          '''
-                    try:
-                        B = cur.execute(sql2, (fbrq,fbusj,blh,nl,xb,jzd,yqtblgx,zwhsjqj,lssj,fbingsj,rysj,rbyy,bk,bzzzytjd))
-                        conn.commit()
-                        print("第"+str(page)+"页第"+str(row)+"条数据插入成功！")
-                    except:
-                        print("第" + str(page) + "页第" + str(row) + "条数据插入失败！")
-                print("第"+str(page)+"页全部插入成功!")
-            print("结束了!")
+            print("需查询至第" + str(Page+1) + "页")
+            # for page in range(1, Page+2):
+            #     url="https://opendata.sz.gov.cn/api/29200_01503668/1/service.xhtml?page="+str(page)+"&rows=200&appKey=d8445b1959af407b9697b67798330930"
+            #     html=urlopen(url)
+            #     hjson=json.loads(html.read())
+            #     #获取data数据
+            #     data = hjson['data']
+            #     print(data)
+            #
+            #     for row in range(0,200):
+            #         fbrq=data[row]['fbrq']
+            #         fbusj=data[row]['fbusj']
+            #         blh=data[row]['blh']
+            #         nl=data[row]['nl']
+            #         xb=data[row]['xb']
+            #         jzd=data[row]['jzd']
+            #         yqtblgx=data[row]['yqtblgx']
+            #         zwhsjqj=data[row]['zwhsjqj']
+            #         lssj=data[row]['lssj']
+            #         fbingsj=data[row]['fbingsj']
+            #         rysj=data[row]['rysj']
+            #         rbyy=data[row]['rbyy']
+            #         bk=data[row]['bk']
+            #         bzzzytjd = data[row]['bzzzytjd']
+            #         sql2 = '''
+            #             insert into covid19(fbrq,fbusj,blh,nl,xb,jzd,yqtblgx,zwhsjqj,lssj,fbingsj,rysj,rbyy,bk,bzzzytjd)
+            #             values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
+            #               '''
+            #         try:
+            #             B = cur.execute(sql2, (fbrq,fbusj,blh,nl,xb,jzd,yqtblgx,zwhsjqj,lssj,fbingsj,rysj,rbyy,bk,bzzzytjd))
+            #             conn.commit()
+            #             print("第"+str(page)+"页第"+str(row)+"条数据插入成功！")
+            #         except:
+            #             print("第" + str(page) + "页第" + str(row) + "条数据插入失败！")
+            #     print("第"+str(page)+"页全部插入成功!")
+            # print("结束了!")
             conn.commit()
             cur.close()
             conn.close()
